3e7e2f48bb8d314dba5c5441f5275fd7d575e7e3
JoshuahMi <38832572+JoshuahMi@users.noreply.github.com>
2026-08-31 23:56:47 +0200

    Did stuff

--- Template/Main/Game/IWO/wb_ws_iwo.gd
+++ Template/Main/Game/IWO/wb_ws_iwo.gd
@@ -9,6 +9,11 @@ class_name WbWsIwo extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	var hud: WbWsHud = get_tree().get_first_node_in_group("Hud") as WbWsHud
+	
+	
+	
 	add_to_group("Iwo")
 	pass # Replace with function body.
 
--- Template/Main/Game/UI/HUD/wb_ws_hud.gd
+++ Template/Main/Game/UI/HUD/wb_ws_hud.gd
@@ -4,6 +4,11 @@ class_name WbWsHud extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("Hud")
+	
+	
+	
+	
+	
 	pass # Replace with function body.
 
 
