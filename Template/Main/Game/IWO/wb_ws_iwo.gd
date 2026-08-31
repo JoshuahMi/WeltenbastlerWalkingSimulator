900df1664ff5d5067b5d264b80d1f9b995d90c00
JoshuahMi <38832572+JoshuahMi@users.noreply.github.com>
2026-08-31 15:00:51 +0200

    Renamed Folder and added stuff

---
+++ Template/Main/Game/IWO/wb_ws_iwo.gd
@@ -0,0 +1,24 @@
+@abstract
+class_name WbWsIwo extends Node3D
+
+## IWO stands for "Important World Object", an object that has Information the
+## HUD can display in the Info Box, optional interactability
+
+## The information about this IWO, to be displayed by the HUD in the info box.
+@export var info: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+@abstract
+func interact()
+
+
+@abstract
+func get_info()
